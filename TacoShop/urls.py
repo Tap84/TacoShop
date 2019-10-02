8c113b5134bad39722a6040d0411dd70065d9667
Tyler Pelton <tap84@case.edu>
2019-10-02 16:07:43 -0400

    updating dependencies to fix import that doesnt exist

--- TacoShop/urls.py
+++ TacoShop/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from tacocreate.views import tacoCreation,tacoDelete
+from tacocreate.views import tacoCreation
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tacoCreation),
